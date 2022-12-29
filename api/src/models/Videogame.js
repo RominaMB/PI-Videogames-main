51a18c5d5107e1102a14a343dea1136d0deb352d
RominaMB <romimolinabravo@gmail.com>
2022-12-29 01:21:17 -0300

    Models update

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,32 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID, // Universal Unique Identifier
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4, // Genera un id por default
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true
     },
+    descriptionS: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    released: {
+      type: DataTypes.STRING 
+    },
+    rating: {
+      type: DataTypes.FLOAT
+    },
+    platforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false
+    },
+  },
+  {
+    timestamps: false
   });
 };

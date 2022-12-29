a8be83a60126a0cbb8ba0677b4b26b2a4b92b18b
RominaMB <romimolinabravo@gmail.com>
2022-12-29 01:33:25 -0300

    Models Update

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -14,7 +14,7 @@ module.exports = (sequelize) => {
       allowNull: false,
       unique: true
     },
-    descriptionS: {
+    descriptions: {
       type: DataTypes.TEXT,
       allowNull: false,
     },

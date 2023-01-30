1b51b4b811b5b619eb8750a193e8632849b2d88a
RominaMB <romimolinabravo@gmail.com>
2023-01-29 22:08:02 -0300

    Update: 29/1 Deploy

--- api/index.js
+++ api/index.js
@@ -20,10 +20,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
-const { DB_PORT } = process.env;
+const { PORT } = process.env;
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(DB_PORT, () => {
-    console.log('%s listening at', DB_PORT); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log('%s listening at', PORT); // eslint-disable-line no-console
   });
 });

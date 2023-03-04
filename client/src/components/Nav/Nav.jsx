d93401237849a721a1a7cf0963d2c13b30354742
RominaMB <romimolinabravo@gmail.com>
2023-03-04 19:57:07 -0300

    Update: 04/03

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 // import SearchBar from '../components/SearchBar';
-import s from './Nav.module.css';
-import nav from '../../assets/nav.png';
+import s from "./Nav.module.css";
+import nav from "../../assets/nav.png";
 
-const Nav = ()=> {
-    return(
-        <>
-            <div className={s.nav__bar}>
-
-                {/* <div className={s.nav__title}> */}
-                    <Link to='/'><img className ={s.nav__img} src={nav} alt='navlanding'/></Link>
-                {/* </div> */}
-                {/* <div className={s.nav__search}><SearchBar/></div> */}
-                <div className={s.nav__alllinks}>
-                    <Link to='/videogames' className={s.nav__link}>HOME</Link>
-                    <Link to='/create' className={s.nav__link}>CREATE</Link>
-                    <Link to='/about' className={s.nav__link}>ABOUT</Link>
-                </div>
-            </div>
-        </>
-    )
-}
+const Nav = () => {
+  return (
+    <>
+      <div className={s.nav__bar}>
+        {/* <div className={s.nav__title}> */}
+        <Link to="/">
+          <img className={s.nav__img} src={nav} alt="navlanding" />
+        </Link>
+        {/* </div> */}
+        {/* <div className={s.nav__search}><SearchBar/></div> */}
+        <div className={s.nav__alllinks}>
+          <Link to="/videogames" className={s.nav__link}>
+            <i class="fa-solid fa-house"></i> HOME
+          </Link>
+          <Link to="/create" className={s.nav__link}>
+            <i class="fa-sharp fa-solid fa-puzzle-piece"></i> CREATE
+          </Link>
+          <Link to="/about" className={s.nav__link}>
+            <i class="fa-solid fa-user"></i> ABOUT
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Nav;

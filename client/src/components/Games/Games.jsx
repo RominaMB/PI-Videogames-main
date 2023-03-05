1efad32d829fdc7abcf5626e5b591cc5bb4d5b7e
RominaMB <romimolinabravo@gmail.com>
2023-03-05 03:27:36 -0300

    Update: 05/03

--- client/src/components/Games/Games.jsx
+++ client/src/components/Games/Games.jsx
@@ -1,56 +1,67 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react'; 
-import { getAllVideogames } from '../../redux/actions';
-import { Link } from 'react-router-dom';
-import s from './Games.module.css';
-import Loading from '../Loading/Loading';
-import Error from '../Error/Error';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { getAllVideogames } from "../../redux/actions";
+import { Link } from "react-router-dom";
+import s from "./Games.module.css";
+import Loading from "../Loading/Loading";
+import Error from "../Error/Error";
 
-const Cards = ()=> {
+const Cards = () => {
+  const dispatch = useDispatch();
+  const allVideogames = useSelector((state) => state.allVideogames);
+  const currentPage = useSelector((state) => state.currentPage);
+  const videogamesPerPage = useSelector((state) => state.videogamesPerPage);
+  const indexOfLastGame = currentPage * videogamesPerPage;
+  const indexOfFirstGame = indexOfLastGame - videogamesPerPage;
+  const currentGames = allVideogames.slice(indexOfFirstGame, indexOfLastGame);
 
-    const dispatch = useDispatch();
-    const allVideogames = useSelector((state)=> state.allVideogames);
-    const currentPage = useSelector((state)=> state.currentPage);
-    const videogamesPerPage = useSelector((state)=> state.videogamesPerPage)
-    const indexOfLastGame = currentPage * videogamesPerPage;
-    const indexOfFirstGame = indexOfLastGame - videogamesPerPage;
-    const currentGames = allVideogames.slice(indexOfFirstGame, indexOfLastGame);
-    
-    const [carga, setCarga] = useState(true);
+  const [carga, setCarga] = useState(true);
 
-    useEffect(()=> {
-        dispatch(getAllVideogames()).then(() => setCarga(false));
-    },[dispatch])
+  useEffect(() => {
+    dispatch(getAllVideogames()).then(() => setCarga(false));
+  }, [dispatch]);
 
-    if (carga) {
-        return <Loading/>
-    }
+  if (carga) {
+    return <Loading />;
+  }
 
-    return(
-        <>
-            <div className={s.card__container}>
-            {currentGames.length > 0 ?
-                (currentGames.map((game)=> {
-                    return(
-                        <div key={game.id} className={s.individual__card}>
-                            <Link to= {`/videogames/${game.id}`}>
-                            <img className={s.game__img} src={game.background_image} alt='game'/>
-                            <div className={s.game__name}>{game.name}</div>
-                            <br></br>
-                            <div className={s.game__genres}>{game.genres?.map(g=> (g.name ? g.name : g)).join(" | ")}</div>
-                            <br></br>
-                            <div className={s.game__rating}>{game.rating}★</div>
-
-                            </Link>  
-                        </div>
-                        )    
-                })) : 
-                <div> <Error/> </div>
-            }
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className={s.card__container}>
+        {currentGames.length > 0 ? (
+          currentGames.map((game) => {
+            return (
+              <div key={game.id} className={s.individual__card}>
+                <Link to={`/videogames/${game.id}`}>
+                  <img
+                    className={s.game__img}
+                    src={game.background_image}
+                    alt="game"
+                  />
+                  <div className={s.game__name}>{game.name}</div>
+                  <br></br>
+                  <div className={s.game__genres}>
+                    {game.genres?.map((g) => (g.name ? g.name : g)).join(" | ")}
+                  </div>
+                  <br></br>
+                  <div className={s.game__rating}>
+                    {game.rating}
+                    <i class="fa-solid fa-star"></i>
+                  </div>
+                </Link>
+              </div>
+            );
+          })
+        ) : (
+          <div>
+            {" "}
+            <Error />{" "}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default Cards;
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,130 +1,149 @@
-import {GET_ALL_VIDEOGAMES, GET_VIDEOGAMES_DETAILS, CLEAN_VIDEOGAMES_DETAILS, GET_GENRES, 
-    SEARCH_GAMES_BY_NAME, ORDER_BY, FILTER_BY_SOURCE, FILTER_BY_GENRE, CHANGE_PAGE} from './actions';
+import {
+  GET_ALL_VIDEOGAMES,
+  GET_VIDEOGAMES_DETAILS,
+  CLEAN_VIDEOGAMES_DETAILS,
+  GET_GENRES,
+  SEARCH_GAMES_BY_NAME,
+  ORDER_BY,
+  FILTER_BY_SOURCE,
+  FILTER_BY_GENRE,
+  CHANGE_PAGE,
+} from "./actions";
 
 //Punto de partida cuando comience la aplicacion
-const initialState = { 
-    allVideogames: [],
-    videogamesForFilter: [],
-    details: [], 
-    genres: [],
-    platforms: [],
+const initialState = {
+  allVideogames: [],
+  videogamesForFilter: [],
+  details: [],
+  genres: [],
+  platforms: [],
 
-    videogamesPerPage: 15, 
-    currentPage: 1,
+  videogamesPerPage: 9,
+  currentPage: 1,
 };
 
-const rootReducer = (state = initialState, action)=> {
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ALL_VIDEOGAMES:
+      return {
+        ...state,
+        allVideogames: action.payload,
+        videogamesForFilter: action.payload,
+      };
+    case GET_VIDEOGAMES_DETAILS:
+      return { ...state, details: [action.payload] };
+    case CLEAN_VIDEOGAMES_DETAILS:
+      return { ...state, details: [] };
+    case GET_GENRES:
+      return { ...state, genres: action.payload };
+    case SEARCH_GAMES_BY_NAME:
+      return { ...state, allVideogames: action.payload, currentPage: 1 };
+    case ORDER_BY:
+      let gamesCopy = [...state.allVideogames];
+      let order;
 
-    switch(action.type) {
-        case GET_ALL_VIDEOGAMES:
-            return {...state, allVideogames: action.payload, videogamesForFilter: action.payload};
-        case GET_VIDEOGAMES_DETAILS:
-            return {...state, details: [action.payload]};
-        case CLEAN_VIDEOGAMES_DETAILS:
-            return {...state, details:[]}
-        case GET_GENRES:
-            return {...state, genres: action.payload};
-        case SEARCH_GAMES_BY_NAME:
-            return{...state, allVideogames: action.payload, currentPage: 1};
-        case ORDER_BY:
-                let gamesCopy = [...state.allVideogames];
-                let order 
-                
-                switch(action.payload){
-                    case 'A-Z':
-                        order = gamesCopy.sort((a,b)=> {
-                        a= a.name.toLowerCase();
-                        b= b.name.toLowerCase();
-                        if(a === b) {
-                            return 0;
-                        }
-                        if (a < b) {
-                            return -1;
-                        }
-                        return 1;
-                    });
-                        break;
-                    case 'Z-A':
-                    order = gamesCopy.sort((a,b)=> {
-                        a= a.name.toLowerCase();
-                        b= b.name.toLowerCase();
-                        if(a === b) {
-                            return 0;
-                        }
-                        if (a > b) {
-                            return -1;
-                        }
-                        return 1;
-                    });
-                        break;
-                    case 'AscRating':
-                        order = gamesCopy.sort((a,b)=> b.rating - a.rating)
-                        break;
-                    case 'DescRating':
-                        order = gamesCopy.sort((a,b)=> a.rating - b.rating)
-                    break;
-                default:
-                    order = gamesCopy;
-                    break;
-        }
-        return {
-            ...state,
-            allVideogames: order,
-            currentPage: 1
-        }
-        case FILTER_BY_SOURCE:
-            let getGames = state.videogamesForFilter;
-            let filter = []
-
-            switch(action.payload){
-                case 'uuid': 
-                filter = getGames.filter(e=> (e.id.length > 5)) // devuelve los uuid, ya que son muchos caracteres.
-                break;
-                case 'api':
-                filter = getGames.filter(e=> typeof(e.id) === 'number') // devuelve los id que son numeros. Typeof indica el tipo de (e.id).
-                break;
-            default:
-                filter = getGames;
-                break;
+      switch (action.payload) {
+        case "A-Z":
+          order = gamesCopy.sort((a, b) => {
+            a = a.name.toLowerCase();
+            b = b.name.toLowerCase();
+            if (a === b) {
+              return 0;
             }
-        return {
-            ...state,
-            allVideogames: filter,
-            currentPage: 1,
-        }
-        case FILTER_BY_GENRE:
-            const videogames = state.videogamesForFilter;
-            const aux = action.payload === 'All Genres' ? videogames : videogames.filter(g => g.genres.some(f => f.name === action.payload));
-            return{ 
-                ...state,
-                allVideogames: aux,
-                currentPage: 1
+            if (a < b) {
+              return -1;
             }
-
-        //condicion --> action.payload es All Genres
-        //expresion 1 --> true, retorna todos los videogames
-        //expresion 2 --> false, filtra los juegos. Por cada juego me fijo en sus generos si hay alguno cuyo nombre es === action.payload,
-        //por cada juego si esta condicion existe pasa el filtro, añadiendose al nuevo array.
-
-        case CHANGE_PAGE:
-            return {
-                ...state,
-                currentPage: Number(action.payload)? parseInt(action.payload) : action.payload === 'Next' ? (parseInt(state.currentPage) + 1 )
-                : (parseInt(state.currentPage) - 1 )
+            return 1;
+          });
+          break;
+        case "Z-A":
+          order = gamesCopy.sort((a, b) => {
+            a = a.name.toLowerCase();
+            b = b.name.toLowerCase();
+            if (a === b) {
+              return 0;
             }
-
-        //condicion action payload es un numero
-        //expresion 1 --> true, convierto action.payload a un num entero
-        //expresion 2 --> false, {
-        //                        condicion action.payload es Next
-        //                        expresion 1 --> true, le sumo 1 al estado de currentPage
-        //                        expresion 2 --> false (es decir es Prev), le resto 1 al estado de currentPage
-        //}                        
-
+            if (a > b) {
+              return -1;
+            }
+            return 1;
+          });
+          break;
+        case "AscRating":
+          order = gamesCopy.sort((a, b) => b.rating - a.rating);
+          break;
+        case "DescRating":
+          order = gamesCopy.sort((a, b) => a.rating - b.rating);
+          break;
         default:
-            return { ...state };
+          order = gamesCopy;
+          break;
+      }
+      return {
+        ...state,
+        allVideogames: order,
+        currentPage: 1,
+      };
+    case FILTER_BY_SOURCE:
+      let getGames = state.videogamesForFilter;
+      let filter = [];
 
-    }
+      switch (action.payload) {
+        case "uuid":
+          filter = getGames.filter((e) => e.id.length > 5); // devuelve los uuid, ya que son muchos caracteres.
+          break;
+        case "api":
+          filter = getGames.filter((e) => typeof e.id === "number"); // devuelve los id que son numeros. Typeof indica el tipo de (e.id).
+          break;
+        default:
+          filter = getGames;
+          break;
+      }
+      return {
+        ...state,
+        allVideogames: filter,
+        currentPage: 1,
+      };
+    case FILTER_BY_GENRE:
+      const videogames = state.videogamesForFilter;
+      const aux =
+        action.payload === "All Genres"
+          ? videogames
+          : videogames.filter((g) =>
+              g.genres.some((f) => f.name === action.payload)
+            );
+      return {
+        ...state,
+        allVideogames: aux,
+        currentPage: 1,
+      };
+
+    //condicion --> action.payload es All Genres
+    //expresion 1 --> true, retorna todos los videogames
+    //expresion 2 --> false, filtra los juegos. Por cada juego me fijo en sus generos si hay alguno cuyo nombre es === action.payload,
+    //por cada juego si esta condicion existe pasa el filtro, añadiendose al nuevo array.
+
+    case CHANGE_PAGE:
+      return {
+        ...state,
+        currentPage: Number(action.payload)
+          ? parseInt(action.payload)
+          : action.payload === "Next"
+          ? parseInt(state.currentPage) + 1
+          : parseInt(state.currentPage) - 1,
+      };
+
+    //condicion action payload es un numero
+    //expresion 1 --> true, convierto action.payload a un num entero
+    //expresion 2 --> false, {
+    //                        condicion action.payload es Next
+    //                        expresion 1 --> true, le sumo 1 al estado de currentPage
+    //                        expresion 2 --> false (es decir es Prev), le resto 1 al estado de currentPage
+    //}
+
+    default:
+      return { ...state };
+  }
 };
 
 export default rootReducer;

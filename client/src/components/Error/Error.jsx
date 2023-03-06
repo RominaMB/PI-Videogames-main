24a7f5efd1b71b3b325478a0d8a0f1d4aa07825a
RominaMB <romimolinabravo@gmail.com>
2023-03-06 18:08:00 -0300

    Update: 06/03

--- client/src/components/Error/Error.jsx
+++ client/src/components/Error/Error.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import notfound from'../../assets/notfound.png';
-import s from './Error.module.css'
+import React from "react";
+import notfound from "../../assets/notfound.png";
+import s from "./Error.module.css";
 
-export default function Error () {
-
-    return (
-        <div className={s.error__container}>
-            <h1 className={s.error__title}>Sorry, not videogames were found.</h1>
-            <img src={notfound} alt='notfound' />
-        </div>
-    )
+export default function Error() {
+  return (
+    <div className={s.error__container}>
+      <h1 className={s.error__title}>Sorry, not videogames were found.</h1>
+      <img className={s.error__img} src={notfound} alt="notfound" />
+    </div>
+  );
 }

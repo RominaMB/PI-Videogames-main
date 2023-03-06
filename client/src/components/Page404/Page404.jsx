8703e42086094d135073788970ef1fad9c3de4b9
RominaMB <romimolinabravo@gmail.com>
2023-03-06 01:49:58 -0300

    Update: 06/03

--- client/src/components/Page404/Page404.jsx
+++ client/src/components/Page404/Page404.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import gameover from'../../assets/404.png';
-import s from './Page404.module.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import gameover from "../../assets/404.png";
+import s from "./Page404.module.css";
 
-export default function NotFound ({location}) {
-
-    return (
-        <div>
-        <div className={s.errorpage__container}>
-            <img className={s.errorpage__img} src={gameover} alt='PageError' />
-            <h2 className={s.errorpage__title}>Sorry, the page you are looking for does not exist ☹ </h2>
-            <h3 className={s.errorpage__details}>The requested URL <code className={s.location__path}>{location.pathname}</code> was not found on this server.</h3>
-            <Link to='/videogames' className={s.errorpage__link}><button className={s.errorpage__btn}>Go Home</button></Link>
-        </div> 
+export default function NotFound({ location }) {
+  return (
+    <div>
+      <div className={s.errorpage__container}>
+        <img className={s.errorpage__img} src={gameover} alt="PageError" />
+        <h2 className={s.errorpage__title}>
+          Sorry, the page you are looking for does not exist ☹{" "}
+        </h2>
+        <h3 className={s.errorpage__details}>
+          The requested URL{" "}
+          <code className={s.location__path}>{location.pathname}</code> was not
+          found on this server.
+        </h3>
+        <Link to="/videogames" className={s.nav__link}>
+          <i class="fa-solid fa-house"></i> BACK HOME
+        </Link>
+      </div>
     </div>
-    )
+  );
 }
